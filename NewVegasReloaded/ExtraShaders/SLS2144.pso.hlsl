mcstfuerson/TES-Reloaded-Shaders

Shaders for TESReloaded

NewVegasReloaded/ExtraShaders/SLS2144.pso.hlsl
@@ -1,0 +1,140 @@
+//
+// Generated by Microsoft (R) HLSL Shader Compiler 9.23.949.2378
+//
+// Parameters:
+
+float4 AmbientColor : register(c1);
+sampler2D BaseMap[7] : register(s0);
+sampler2D NormalMap[7] : register(s7);
+float4 PSLightColor[10] : register(c3);
+float4 PSLightDir : register(c18);
+float4 PSLightPosition[8] : register(c19);
+float4 TESR_ShadowData : register(c32);
+sampler2D TESR_ShadowMapBuffer : register(s14) = sampler_state { ADDRESSU = CLAMP; ADDRESSV = CLAMP; MAGFILTER = LINEAR; MINFILTER = LINEAR; MIPFILTER = LINEAR; };
+
+// Registers:
+//
+//   Name            Reg   Size
+//   --------------- ----- ----
+//   AmbientColor    const_1       1
+//   PSLightColor[0]    const_3       4
+//   PSLightDir      const_18      1
+//   PSLightPosition[0] const_19      3
+//   BaseMap         texture_0       7
+//   NormalMap       texture_7       7
+//
+
+
+// Structures:
+
+struct VS_INPUT {
+	float3 LCOLOR_0 : COLOR0;
+	float4 LCOLOR_1 : COLOR1;
+    float2 BaseUV : TEXCOORD0;
+    float3 texcoord_1 : TEXCOORD1_centroid;
+	float3 texcoord_2 : TEXCOORD2_centroid;
+    float3 texcoord_3 : TEXCOORD3_centroid;
+    float3 texcoord_4 : TEXCOORD4_centroid;
+    float3 texcoord_5 : TEXCOORD5_centroid;
+	float4 texcoord_6 : TEXCOORD6;
+    float4 texcoord_7 : TEXCOORD7_centroid;
+};
+
+struct PS_OUTPUT {
+    float4 color_0 : COLOR0;
+};
+
+#include "../Shadows/Includes/Shadow.hlsl"
+
+PS_OUTPUT main(VS_INPUT IN) {
+    PS_OUTPUT OUT;
+
+#define	expand(v)		(((v) - 0.5) / 0.5)
+#define	compress(v)		(((v) * 0.5) + 0.5)
+#define	shade(n, l)		max(dot(n, l), 0)
+#define	shades(n, l)		saturate(dot(n, l))
+
+    float3 m61;
+    float3 m63;
+    float3 m65;
+    float3 m69;
+    float3 q0;
+    float3 q1;
+    float3 q10;
+    float3 q11;
+    float3 q12;
+    float3 q16;
+    float3 q17;
+    float3 q2;
+    float3 q28;
+    float3 q29;
+    float3 q3;
+    float3 q4;
+    float3 q5;
+    float3 q6;
+    float3 q7;
+    float3 q8;
+    float3 q84;
+    float3 q9;
+    float3 q92;
+    float4 r0;
+    float4 r1;
+    float4 r10;
+    float4 r11;
+    float4 r12;
+    float4 r13;
+    float4 r2;
+    float4 r3;
+    float4 r4;
+    float4 r5;
+    float4 r6;
+    float4 r7;
+    float4 r8;
+    float4 r9;
+
+    r2.xyzw = tex2D(NormalMap[2], IN.BaseUV.xy);
+    r0.xyzw = tex2D(NormalMap[1], IN.BaseUV.xy);
+    r1.xyzw = tex2D(NormalMap[0], IN.BaseUV.xy);
+    r13.xyzw = tex2D(BaseMap[6], IN.BaseUV.xy);
+    r12.xyzw = tex2D(BaseMap[5], IN.BaseUV.xy);
+    r11.xyzw = tex2D(BaseMap[4], IN.BaseUV.xy);
+    r10.xyzw = tex2D(BaseMap[3], IN.BaseUV.xy);
+    r9.xyzw = tex2D(BaseMap[2], IN.BaseUV.xy);
+    r6.xyzw = tex2D(NormalMap[6], IN.BaseUV.xy);
+    r5.xyzw = tex2D(NormalMap[5], IN.BaseUV.xy);
+    r4.xyzw = tex2D(NormalMap[4], IN.BaseUV.xy);
+    r3.xyzw = tex2D(NormalMap[3], IN.BaseUV.xy);
+    r7.xyzw = tex2D(BaseMap[1], IN.BaseUV.xy);
+    r8.xyzw = tex2D(BaseMap[0], IN.BaseUV.xy);
+    q2.xyz = normalize(IN.texcoord_5.xyz);
+    q1.xyz = normalize(IN.texcoord_4.xyz);
+    q3.xyz = normalize(IN.texcoord_3.xyz);
+    m63.xyz = mul(float3x3(q3.xyz, q1.xyz, q2.xyz), PSLightDir.xyz);
+    q7.xyz = PSLightPosition[2].xyz - IN.texcoord_2.xyz;
+    q8.xyz = q7.xyz / PSLightPosition[2].w;
+    m69.xyz = mul(float3x3(q3.xyz, q1.xyz, q2.xyz), q7.xyz);
+    q5.xyz = PSLightPosition[1].xyz - IN.texcoord_2.xyz;
+    q6.xyz = q5.xyz / PSLightPosition[1].w;
+    m65.xyz = mul(float3x3(q3.xyz, q1.xyz, q2.xyz), q5.xyz);
+    q0.xyz = PSLightPosition[0].xyz - IN.texcoord_2.xyz;
+    q4.xyz = q0.xyz / PSLightPosition[0].w;
+    m61.xyz = mul(float3x3(q3.xyz, q1.xyz, q2.xyz), q0.xyz);
+    q9.xyz = (IN.LCOLOR_0.z * r9.xyz) + ((IN.LCOLOR_0.x * r8.xyz) + (r7.xyz * IN.LCOLOR_0.y));
+    q10.xyz = (IN.LCOLOR_1.z * r12.xyz) + ((IN.LCOLOR_1.y * r11.xyz) + ((IN.LCOLOR_1.x * r10.xyz) + q9.xyz));
+    r0.xyz = (2 * ((r1.xyz - 0.5) * IN.LCOLOR_0.x)) + (2 * ((r0.xyz - 0.5) * IN.LCOLOR_0.y));	// [0,1] to [-1,+1]
+    q16.xyz = (2 * ((r3.xyz - 0.5) * IN.LCOLOR_1.x)) + ((2 * ((r2.xyz - 0.5) * IN.LCOLOR_0.z)) + r0.xyz);	// [0,1] to [-1,+1]
+    q17.xyz = (2 * ((r5.xyz - 0.5) * IN.LCOLOR_1.z)) + ((2 * ((r4.xyz - 0.5) * IN.LCOLOR_1.y)) + q16.xyz);	// [0,1] to [-1,+1]
+    q84.xyz = normalize((2 * ((r6.xyz - 0.5) * IN.LCOLOR_1.w)) + q17.xyz);	// [0,1] to [-1,+1]
+    q92.xyz = shades(q84.xyz, normalize(m61.xyz)) * (1 - shades(q4.xyz, q4.xyz)) * PSLightColor[1].xyz;
+    r2.xyz = (shades(q84.xyz, m63.xyz) * PSLightColor[0].rgb) + q92.xyz;
+    q28.xyz = ((shades(q84.xyz, normalize(m65.xyz)) * (1 - shades(q6.xyz, q6.xyz))) * PSLightColor[2].xyz) + r2.xyz;
+    q29.xyz = ((shades(q84.xyz, normalize(m69.xyz)) * (1 - shades(q8.xyz, q8.xyz))) * PSLightColor[3].xyz) + q28.xyz;
+    q11.xyz = ((GetLightAmount(IN.texcoord_6) * q29.xyz) + AmbientColor.rgb) * ((IN.LCOLOR_1.w * r13.xyz) + q10.xyz);
+    q12.xyz = (IN.texcoord_7.w * (IN.texcoord_7.xyz - (IN.texcoord_1.xyz * q11.xyz))) + (q11.xyz * IN.texcoord_1.xyz);
+    OUT.color_0.a = 1;
+    OUT.color_0.rgb = q12.xyz;
+
+    return OUT;
+};
+
+// approximately 109 instruction slots used (14 texture, 95 arithmetic)

OblivionReloaded/Grass/GRASS2030.vso.hlsl
@@ -1,0 +1,134 @@
+//
+// Generated by Microsoft (R) D3DX9 Shader Compiler 9.08.299.0000
+//
+//   vsa shaderdump19/GRASS2030.vso /Fcshaderdump19/GRASS2030.vso.dis
+//
+//
+// Parameters:
+//
+float4 AddlParams : register(c7);
+float4 AlphaParam : register(c5);
+float4 AmbientColor : register(c6);
+float3 DiffuseColor : register(c1);
+float3 DiffuseDir : register(c0);
+float4 FogColor : register(c14);
+float4 FogParam : register(c15);
+float4 InstanceData[2] : register(c20);
+row_major float4x4 ModelViewProj : register(c9);
+float3 ScaleMask : register(c2);
+float4 ShadowProjData : register(c8);
+float4 ShadowProjTransform : register(c13);
+float4 WindData : register(c4);
+float4 TESR_GrassScale : register(c248);
+row_major float4x4 TESR_ShadowCameraToLightTransform : register(c249);
+
+//
+//
+// Registers:
+//
+//   Name                Reg   Size
+//   ------------------- ----- ----
+//   DiffuseDir          const_0       1
+//   DiffuseColor        const_1       1
+//   ScaleMask           const_2       1
+//   WindData            const_4       1
+//   AlphaParam          const_5       1
+//   AmbientColor        const_6       1
+//   AddlParams          const_7       1
+//   ShadowProjData      const_8       1
+//   ModelViewProj[0]       const_9        1
+//   ModelViewProj[1]       const_10        1
+//   ModelViewProj[2]       const_11        1
+//   ModelViewProj[3]       const_12        1
+//   ShadowProjTransform const_13      1
+//   FogColor            const_14      1
+//   FogParam            const_15      1
+//   InstanceData[0]        const_20       1
+//   InstanceData[1]        const_21       1
+//
+
+
+// Structures:
+
+struct VS_INPUT {
+    float4 position : POSITION;
+    float4 color_0 : COLOR0;
+    float4 texcoord_0 : TEXCOORD0;
+    float4 texcoord_1 : TEXCOORD1;
+};
+
+struct VS_OUTPUT {
+    float4 color_0 : COLOR0;
+    float4 position : POSITION;
+    float2 texcoord_0 : TEXCOORD0;
+    float4 texcoord_2 : TEXCOORD2;
+    float4 texcoord_4 : TEXCOORD4;
+    float4 texcoord_5 : TEXCOORD5;
+};
+
+// Code:
+
+VS_OUTPUT main(VS_INPUT IN) {
+    VS_OUTPUT OUT;
+
+#define	PI				3.14159274
+#define	anglei(v)		(((v) + PI) / (2 * PI))
+#define	angler(v)		(((v) * (2 * PI)) - PI)
+#define	fracr(v)		angler(frac(anglei(v)))	// signed modulo % PI
+#define	expand(v)		(((v) - 0.5) / 0.5)
+#define	shades(n, l)	saturate(dot(n, l))
+#define	sqr(v)			((v) * (v))
+
+    const int4 const_16 = {0, 2, -1, 1};
+	
+    float4 q0;
+    float4 r0;
+    float4 r1;
+    float4 r2;
+    float2 r3;
+    float3 r4;
+	float3 scale;
+	
+    r1.xyzw = frac(InstanceData[0 + IN.texcoord_1.x]);
+    r1.xyz = r1.xyz - 0.5;
+    q0.x = InstanceData[0 + IN.texcoord_1.x].y + InstanceData[0 + IN.texcoord_1.x].x;
+    OUT.color_0.rgb = FogColor.rgb;
+    OUT.texcoord_0.xy = IN.texcoord_0.xy;
+    r0.xyz = 2 * r1.xyz;
+    r2.xyz = abs(r0.xyz);
+    r0.w = -r0.y;
+    r2.xyzw = (r2.yzxz >= r2.xxyy ? 1.0 : 0.0);
+    r3.xy = r2.yw * r2.xz;
+    r1.x = (2 * r1.y) - r0.z;
+    r1.yz = r0.x * const_16.zw;
+    r2.z = r3.y * r1.z;
+    r2.xy = (r3.y * r1.xy) + r0.wx;
+    r1.x = -r2.x;
+    r1.yz = (r0.zy * const_16.zw) - r2.yz;
+    r1.xyz = (r3.x * r1.xyz) + r2.xyz;
+    r4.xyz = normalize(r1.xyz);
+    r2.x = expand(frac(q0.x / 17));
+    r2.w = sqrt(1.0 - sqr(r2.x));
+    r2.z = 0;
+    r2.y = -r2.w;
+	scale.xyz = ScaleMask.xyz * TESR_GrassScale.xyz;
+    r1.xyz = (((0.01 * InstanceData[0 + IN.texcoord_1.x].w) * scale.xyz) + TESR_GrassScale.w) * IN.position.xyz;
+    r1.xyz = (r1.z * r0.xyz) + ((dot(r2.xyz, r1.xyz) * cross(r4.xyz, r0.xyz)) + (r4.xyz * dot(r2.wxz, r1.xyz)));
+    r2.xyz = ((r1.w * IN.color_0.rgb) * shades(DiffuseDir.xyz, r0.xyz)) * DiffuseColor.rgb;
+    r1.w = IN.position.w;
+    r1.xy = (((sin(fracr((q0.x / 128) + WindData.w)) * WindData.z) * sqr(IN.color_0.a)) * WindData.xy) + r1.xy;
+    r1.xyz = r1.xyz + InstanceData[0 + IN.texcoord_1.x].xyz;
+    r0 = mul(ModelViewProj, r1.xyzw);
+	q0 = mul(r0, TESR_ShadowCameraToLightTransform);
+	r1.xy = saturate((length(r0.xyzw) - AlphaParam.xz) / AlphaParam.yw);
+	OUT.color_0.a = 1 - saturate((FogParam.x - length(r0.xyz)) / FogParam.y);
+	OUT.position.xyzw = r0.xyzw;
+    OUT.texcoord_2 = q0;
+    OUT.texcoord_4.xyzw = AmbientColor.rgba;
+    OUT.texcoord_5.w = r1.x * (1 - r1.y);
+    OUT.texcoord_5.xyz = r2.xyz * AddlParams.x;
+
+    return OUT;
+};
+
+// approximately 103 instruction slots used

DethRaid/TES-Reloaded-Shaders-NEW

NewVegasReloaded/ExtraShaders/SLS2144.pso.hlsl
@@ -1,0 +1,142 @@
+//
+// Generated by Microsoft (R) HLSL Shader Compiler 9.23.949.2378
+//
+// Parameters:
+
+float4 AmbientColor : register(c1);
+sampler2D BaseMap[7] : register(s0);
+sampler2D NormalMap[7] : register(s7);
+float4 PSLightColor[10] : register(c3);
+float4 TESR_FogColor : register(c15);
+float4 PSLightDir : register(c18);
+float4 PSLightPosition[8] : register(c19);
+float4 TESR_ShadowData : register(c32);
+sampler2D TESR_ShadowMapBufferNear : register(s14) = sampler_state { ADDRESSU = CLAMP; ADDRESSV = CLAMP; MAGFILTER = LINEAR; MINFILTER = LINEAR; MIPFILTER = LINEAR; };
+sampler2D TESR_ShadowMapBufferFar : register(s15) = sampler_state { ADDRESSU = CLAMP; ADDRESSV = CLAMP; MAGFILTER = LINEAR; MINFILTER = LINEAR; MIPFILTER = LINEAR; };
+
+// Registers:
+//
+//   Name            Reg   Size
+//   --------------- ----- ----
+//   AmbientColor    const_1       1
+//   PSLightColor[0]    const_3       4
+//   PSLightDir      const_18      1
+//   PSLightPosition[0] const_19      3
+//   BaseMap         texture_0       7
+//   NormalMap       texture_7       7
+//
+
+
+// Structures:
+
+struct VS_INPUT {
+	float3 LCOLOR_0 : COLOR0;
+	float4 LCOLOR_1 : COLOR1;
+    float3 BaseUV : TEXCOORD0;
+    float3 texcoord_1 : TEXCOORD1_centroid;
+	float3 texcoord_2 : TEXCOORD2_centroid;
+    float3 texcoord_3 : TEXCOORD3_centroid;
+    float3 texcoord_4 : TEXCOORD4_centroid;
+    float3 texcoord_5 : TEXCOORD5_centroid;
+	float4 texcoord_6 : TEXCOORD6;
+    float4 texcoord_7 : TEXCOORD7;
+};
+
+struct PS_OUTPUT {
+    float4 color_0 : COLOR0;
+};
+
+#include "../Shadows/Includes/Shadow.hlsl"
+
+PS_OUTPUT main(VS_INPUT IN) {
+    PS_OUTPUT OUT;
+
+#define	expand(v)		(((v) - 0.5) / 0.5)
+#define	compress(v)		(((v) * 0.5) + 0.5)
+#define	shade(n, l)		max(dot(n, l), 0)
+#define	shades(n, l)		saturate(dot(n, l))
+
+    float3 m61;
+    float3 m63;
+    float3 m65;
+    float3 m69;
+    float3 q0;
+    float3 q1;
+    float3 q10;
+    float3 q11;
+    float3 q12;
+    float3 q16;
+    float3 q17;
+    float3 q2;
+    float3 q28;
+    float3 q29;
+    float3 q3;
+    float3 q4;
+    float3 q5;
+    float3 q6;
+    float3 q7;
+    float3 q8;
+    float3 q84;
+    float3 q9;
+    float3 q92;
+    float4 r0;
+    float4 r1;
+    float4 r10;
+    float4 r11;
+    float4 r12;
+    float4 r13;
+    float4 r2;
+    float4 r3;
+    float4 r4;
+    float4 r5;
+    float4 r6;
+    float4 r7;
+    float4 r8;
+    float4 r9;
+
+    r2.xyzw = tex2D(NormalMap[2], IN.BaseUV.xy);
+    r0.xyzw = tex2D(NormalMap[1], IN.BaseUV.xy);
+    r1.xyzw = tex2D(NormalMap[0], IN.BaseUV.xy);
+    r13.xyzw = tex2D(BaseMap[6], IN.BaseUV.xy);
+    r12.xyzw = tex2D(BaseMap[5], IN.BaseUV.xy);
+    r11.xyzw = tex2D(BaseMap[4], IN.BaseUV.xy);
+    r10.xyzw = tex2D(BaseMap[3], IN.BaseUV.xy);
+    r9.xyzw = tex2D(BaseMap[2], IN.BaseUV.xy);
+    r6.xyzw = tex2D(NormalMap[6], IN.BaseUV.xy);
+    r5.xyzw = tex2D(NormalMap[5], IN.BaseUV.xy);
+    r4.xyzw = tex2D(NormalMap[4], IN.BaseUV.xy);
+    r3.xyzw = tex2D(NormalMap[3], IN.BaseUV.xy);
+    r7.xyzw = tex2D(BaseMap[1], IN.BaseUV.xy);
+    r8.xyzw = tex2D(BaseMap[0], IN.BaseUV.xy);
+    q2.xyz = normalize(IN.texcoord_5.xyz);
+    q1.xyz = normalize(IN.texcoord_4.xyz);
+    q3.xyz = normalize(IN.texcoord_3.xyz);
+    m63.xyz = mul(float3x3(q3.xyz, q1.xyz, q2.xyz), PSLightDir.xyz);
+    q7.xyz = PSLightPosition[2].xyz - IN.texcoord_2.xyz;
+    q8.xyz = q7.xyz / PSLightPosition[2].w;
+    m69.xyz = mul(float3x3(q3.xyz, q1.xyz, q2.xyz), q7.xyz);
+    q5.xyz = PSLightPosition[1].xyz - IN.texcoord_2.xyz;
+    q6.xyz = q5.xyz / PSLightPosition[1].w;
+    m65.xyz = mul(float3x3(q3.xyz, q1.xyz, q2.xyz), q5.xyz);
+    q0.xyz = PSLightPosition[0].xyz - IN.texcoord_2.xyz;
+    q4.xyz = q0.xyz / PSLightPosition[0].w;
+    m61.xyz = mul(float3x3(q3.xyz, q1.xyz, q2.xyz), q0.xyz);
+    q9.xyz = (IN.LCOLOR_0.z * r9.xyz) + ((IN.LCOLOR_0.x * r8.xyz) + (r7.xyz * IN.LCOLOR_0.y));
+    q10.xyz = (IN.LCOLOR_1.z * r12.xyz) + ((IN.LCOLOR_1.y * r11.xyz) + ((IN.LCOLOR_1.x * r10.xyz) + q9.xyz));
+    r0.xyz = (2 * ((r1.xyz - 0.5) * IN.LCOLOR_0.x)) + (2 * ((r0.xyz - 0.5) * IN.LCOLOR_0.y));	// [0,1] to [-1,+1]
+    q16.xyz = (2 * ((r3.xyz - 0.5) * IN.LCOLOR_1.x)) + ((2 * ((r2.xyz - 0.5) * IN.LCOLOR_0.z)) + r0.xyz);	// [0,1] to [-1,+1]
+    q17.xyz = (2 * ((r5.xyz - 0.5) * IN.LCOLOR_1.z)) + ((2 * ((r4.xyz - 0.5) * IN.LCOLOR_1.y)) + q16.xyz);	// [0,1] to [-1,+1]
+    q84.xyz = normalize((2 * ((r6.xyz - 0.5) * IN.LCOLOR_1.w)) + q17.xyz);	// [0,1] to [-1,+1]
+    q92.xyz = shades(q84.xyz, normalize(m61.xyz)) * (1 - shades(q4.xyz, q4.xyz)) * PSLightColor[1].xyz;
+    r2.xyz = (shades(q84.xyz, m63.xyz) * PSLightColor[0].rgb) + q92.xyz;
+    q28.xyz = ((shades(q84.xyz, normalize(m65.xyz)) * (1 - shades(q6.xyz, q6.xyz))) * PSLightColor[2].xyz) + r2.xyz;
+    q29.xyz = ((shades(q84.xyz, normalize(m69.xyz)) * (1 - shades(q8.xyz, q8.xyz))) * PSLightColor[3].xyz) + q28.xyz;
+    q11.xyz = ((GetLightAmount(IN.texcoord_6, IN.texcoord_7) * q29.xyz) + AmbientColor.rgb) * ((IN.LCOLOR_1.w * r13.xyz) + q10.xyz);
+    q12.xyz = (IN.BaseUV.z * (TESR_FogColor.xyz - (IN.texcoord_1.xyz * q11.xyz))) + (q11.xyz * IN.texcoord_1.xyz);
+    OUT.color_0.a = 1;
+    OUT.color_0.rgb = q12.xyz;
+
+    return OUT;
+};
+
+// approximately 109 instruction slots used (14 texture, 95 arithmetic)

OblivionReloaded/Grass/GRASS2030.vso.hlsl
@@ -1,0 +1,133 @@
+//
+// Generated by Microsoft (R) D3DX9 Shader Compiler 9.08.299.0000
+//
+//   vsa shaderdump19/GRASS2030.vso /Fcshaderdump19/GRASS2030.vso.dis
+//
+//
+// Parameters:
+//
+float3 DiffuseDir : register(c0);
+float3 DiffuseColor : register(c1);
+float3 ScaleMask : register(c2);
+float4 WindData : register(c4);
+float4 AlphaParam : register(c5);
+float4 AmbientColor : register(c6);
+float4 AddlParams : register(c7);
+float4 ShadowProjData : register(c8);
+row_major float4x4 ModelViewProj : register(c9);
+float4 ShadowProjTransform : register(c13);
+float4 FogColor : register(c14);
+float4 FogParam : register(c15);
+float4 InstanceData[228] : register(c20);
+float4 TESR_GrassScale : register(c248);
+row_major float4x4 TESR_ShadowCameraToLightTransform : register(c249);
+
+//
+//
+// Registers:
+//
+//   Name                Reg   Size
+//   ------------------- ----- ----
+//   DiffuseDir          const_0       1
+//   DiffuseColor        const_1       1
+//   ScaleMask           const_2       1
+//   WindData            const_4       1
+//   AlphaParam          const_5       1
+//   AmbientColor        const_6       1
+//   AddlParams          const_7       1
+//   ShadowProjData      const_8       1
+//   ModelViewProj[0]       const_9        1
+//   ModelViewProj[1]       const_10        1
+//   ModelViewProj[2]       const_11        1
+//   ModelViewProj[3]       const_12        1
+//   ShadowProjTransform const_13      1
+//   FogColor            const_14      1
+//   FogParam            const_15      1
+//   InstanceData[0]        const_20       1
+//   InstanceData[1]        const_21       1
+//
+
+
+// Structures:
+
+struct VS_INPUT {
+    float4 position : POSITION;
+    float4 color_0 : COLOR0;
+    float4 texcoord_0 : TEXCOORD0;
+    float4 texcoord_1 : TEXCOORD1;
+};
+
+struct VS_OUTPUT {
+    float4 color_0 : COLOR0;
+    float4 position : POSITION;
+    float2 texcoord_0 : TEXCOORD0;
+    float3 texcoord_4 : TEXCOORD4;
+	float4 texcoord_5 : TEXCOORD5;
+	float4 texcoord_6 : TEXCOORD6;
+};
+
+// Code:
+
+VS_OUTPUT main(VS_INPUT IN) {
+    VS_OUTPUT OUT;
+
+#define	PI				3.14159274
+#define	anglei(v)		(((v) + PI) / (2 * PI))
+#define	angler(v)		(((v) * (2 * PI)) - PI)
+#define	fracr(v)		angler(frac(anglei(v)))	// signed modulo % PI
+#define	expand(v)		(((v) - 0.5) / 0.5)
+#define	shades(n, l)	saturate(dot(n, l))
+#define	sqr(v)			((v) * (v))
+
+    const int4 const_16 = {0, 2, -1, 1};
+	
+    float4 q0;
+    float4 r0;
+    float4 r1;
+    float4 r2;
+    float2 r3;
+    float3 r4;
+	float3 scale;
+	
+    r1.xyzw = frac(InstanceData[0 + IN.texcoord_1.x]);
+    r1.xyz = r1.xyz - 0.5;
+    q0.x = InstanceData[0 + IN.texcoord_1.x].y + InstanceData[0 + IN.texcoord_1.x].x;
+    OUT.color_0.rgb = FogColor.rgb;
+    OUT.texcoord_0.xy = IN.texcoord_0.xy;
+    r0.xyz = 2 * r1.xyz;
+    r2.xyz = abs(r0.xyz);
+    r0.w = -r0.y;
+    r2.xyzw = (r2.yzxz >= r2.xxyy ? 1.0 : 0.0);
+    r3.xy = r2.yw * r2.xz;
+    r1.x = (2 * r1.y) - r0.z;
+    r1.yz = r0.x * const_16.zw;
+    r2.z = r3.y * r1.z;
+    r2.xy = (r3.y * r1.xy) + r0.wx;
+    r1.x = -r2.x;
+    r1.yz = (r0.zy * const_16.zw) - r2.yz;
+    r1.xyz = (r3.x * r1.xyz) + r2.xyz;
+    r4.xyz = normalize(r1.xyz);
+    r2.x = expand(frac(q0.x / 17));
+    r2.w = sqrt(1.0 - sqr(r2.x));
+    r2.z = 0;
+    r2.y = -r2.w;
+	scale.xyz = ScaleMask.xyz * TESR_GrassScale.xyz;
+    r1.xyz = (((0.01 * InstanceData[0 + IN.texcoord_1.x].w) * scale.xyz) + TESR_GrassScale.w) * IN.position.xyz;
+    r1.xyz = (r1.z * r0.xyz) + ((dot(r2.xyz, r1.xyz) * cross(r4.xyz, r0.xyz)) + (r4.xyz * dot(r2.wxz, r1.xyz)));
+    r2.xyz = ((r1.w * IN.color_0.rgb) * shades(DiffuseDir.xyz, r0.xyz)) * DiffuseColor.rgb;
+    r1.w = IN.position.w;
+    r1.xy = (((sin(fracr((q0.x / 128) + WindData.w)) * WindData.z) * sqr(IN.color_0.a)) * WindData.xy) + r1.xy;
+    r1.xyz = r1.xyz + InstanceData[0 + IN.texcoord_1.x].xyz;
+    r0 = mul(ModelViewProj, r1);
+	r1.xy = saturate((length(r0.xyzw) - AlphaParam.xz) / AlphaParam.yw);
+	OUT.color_0.a = 1 - saturate((FogParam.x - length(r0.xyz)) / FogParam.y);
+	OUT.position = r0;
+    OUT.texcoord_4.xyz = AmbientColor.rgb;
+    OUT.texcoord_5.w = r1.x * (1 - r1.y);
+    OUT.texcoord_5.xyz = r2.xyz * AddlParams.x;
+    OUT.texcoord_6 = mul(r0, TESR_ShadowCameraToLightTransform);
+	
+    return OUT;
+};
+
+// approximately 103 instruction slots used