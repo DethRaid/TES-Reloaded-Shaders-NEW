36e21c67228746627acae12609d73ae8b5a40f4b
David Dubois <dexcelstraun7@gmail.com>
2021-01-31 11:36:57 -0800

    Began adding RSM, refactored for clarity

--- NewVegasReloaded/Shadows/ShadowMap.pso.hlsl
+++ NewVegasReloaded/Shadows/ShadowMap.pso.hlsl
@@ -9,23 +9,23 @@ struct VS_OUTPUT {
 
 struct PS_OUTPUT {
     float4 color_0 : COLOR0;
+	float4 color_specular : COLOR1;
+	float4 normal_roughness : COLOR2;
 };
 
 
 PS_OUTPUT main(VS_OUTPUT IN) {
     PS_OUTPUT OUT;
-	
-	float4 r0;
-	float r1;
-	
+		
 	if (TESR_ShadowData.x == 2.0f || TESR_ShadowData.y == 1.0f) { // Leaves (Speedtrees) or alpha is required
-		r0.rgba = tex2D(DiffuseMap, IN.texcoord_1.xy);
-		if (r0.a > 0.2f) 
-			r1 = IN.texcoord_0.z / IN.texcoord_0.w;
-		else
+		float4 r0 = tex2D(DiffuseMap, IN.texcoord_1.xy);
+		if (r0.a <= 0.2f) {
 			discard;
-		OUT.color_0 = r1;
-		return OUT;
+		}
+		
+		if (TESR_ShadowData.z < 0.5f) {	// Reflectance Shadow Maps are enabled
+			OUT.color_specular.rgb = DiffuseColor.rgb;
+		}
 	}
     OUT.color_0 = IN.texcoord_0.z / IN.texcoord_0.w;
     return OUT;
