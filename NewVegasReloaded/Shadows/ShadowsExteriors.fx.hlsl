f1e6a06a200a34e92ed1aaef5deec0b0edbba897
David Dubois <dexcelstraun7@gmail.com>
2021-01-31 18:00:12 -0800

    Reformat the file

--- NewVegasReloaded/Shadows/ShadowsExteriors.fx.hlsl
+++ NewVegasReloaded/Shadows/ShadowsExteriors.fx.hlsl
@@ -68,21 +68,18 @@ static const float2 BlurOffsets[cKernelSize] =
 	float2( 3.0f * TESR_ReciprocalResolution.x,  3.0f * TESR_ReciprocalResolution.y),
 };
 
-float readDepth(in float2 coord : TEXCOORD0)
-{
+float readDepth(in float2 coord : TEXCOORD0) {
 	float posZ = tex2D(TESR_DepthBuffer, coord).x;
 	posZ = Zmul / ((posZ * Zdiff) - farZ);
 	return posZ;
 }
 
-float readDepth01(in float2 coord : TEXCOORD0)
-{
+float readDepth01(in float2 coord : TEXCOORD0) {
 	float posZ = tex2D(TESR_DepthBuffer, coord).x;
 	return (2.0f * nearZ) / (nearZ + farZ - posZ * (farZ - nearZ));
 }
 
-float3 toWorld(float2 tex)
-{
+float3 toWorld(float2 tex) {
     float3 v = float3(TESR_ViewTransform[0][2], TESR_ViewTransform[1][2], TESR_ViewTransform[2][2]);
     v += (1 / TESR_ProjectionTransform[0][0] * (2 * tex.x - 1)).xxx * float3(TESR_ViewTransform[0][0], TESR_ViewTransform[1][0], TESR_ViewTransform[2][0]);
     v += (-1 / TESR_ProjectionTransform[1][1] * (2 * tex.y - 1)).xxx * float3(TESR_ViewTransform[0][1], TESR_ViewTransform[1][1], TESR_ViewTransform[2][1]);
@@ -90,57 +87,56 @@ float3 toWorld(float2 tex)
 }
 
 float LookupFar(float4 ShadowPos) {
-	
 	float Shadow = tex2D(TESR_ShadowMapBufferFar, ShadowPos.xy).r;
-	if (Shadow < ShadowPos.z - BIAS) return TESR_ShadowData.y;
+	if (Shadow < ShadowPos.z - BIAS) {
+		return TESR_ShadowData.y;
+	}
+
 	return 1.0f;
-	
 }
 
 float GetLightAmountFar(float4 ShadowPos) {
-
 	float x;
 	float y;
 	
-	ShadowPos.xyz /= ShadowPos.w;
     if (ShadowPos.x < -1.0f || ShadowPos.x > 1.0f ||
         ShadowPos.y < -1.0f || ShadowPos.y > 1.0f ||
-        ShadowPos.z <  0.0f || ShadowPos.z > 1.0f)
+        ShadowPos.z <  0.0f || ShadowPos.z > 1.0f) {
 		return 1.0f;
+	}
 
     ShadowPos.x = ShadowPos.x *  0.5f + 0.5f;
     ShadowPos.y = ShadowPos.y * -0.5f + 0.5f;
-	return LookupFar(ShadowPos);
 	
+	return LookupFar(ShadowPos);
 }
 
 float Lookup(float4 ShadowPos) {
-	
 	float Shadow = tex2D(TESR_ShadowMapBufferNear, ShadowPos.xy).r;
-	if (Shadow < ShadowPos.z - BIAS) return TESR_ShadowData.y;
+	if (Shadow < ShadowPos.z - BIAS) {
+		return TESR_ShadowData.y;
+	}
+
 	return 1.0f;
-	
 }
 
-float GetLightAmount(float4 ShadowPos, float4 ShadowPosFar) {
-	
+float GetLightAmount(float4 ShadowPos, float4 ShadowPosFar) {	
 	float x;
 	float y;
 	
-	ShadowPos.xyz /= ShadowPos.w;
     if (ShadowPos.x < -1.0f || ShadowPos.x > 1.0f ||
         ShadowPos.y < -1.0f || ShadowPos.y > 1.0f ||
-        ShadowPos.z <  0.0f || ShadowPos.z > 1.0f)
+        ShadowPos.z <  0.0f || ShadowPos.z > 1.0f) {
 		return GetLightAmountFar(ShadowPosFar);
+	}
  
     ShadowPos.x = ShadowPos.x *  0.5f + 0.5f;
     ShadowPos.y = ShadowPos.y * -0.5f + 0.5f;
-	return Lookup(ShadowPos);
 	
+	return Lookup(ShadowPos);
 }
 
-float4 Shadow( VSOUT IN ) : COLOR0 {
-	
+float4 Shadow( VSOUT IN ) : COLOR0 {	
 	float Shadow = 1.0f;
 	float depth = readDepth(IN.UVCoord);
     float3 camera_vector = toWorld(IN.UVCoord) * depth;
@@ -148,8 +144,13 @@ float4 Shadow( VSOUT IN ) : COLOR0 {
 	
 	if (world_pos.z > TESR_WaterSettings.x) {
 		float4 pos = mul(world_pos, TESR_WorldViewProjectionTransform);
+
 		float4 ShadowNear = mul(pos, TESR_ShadowCameraToLightTransformNear);
+		ShadowNear /=  ShadowNear.w;
+
 		float4 ShadowFar = mul(pos, TESR_ShadowCameraToLightTransformFar);	
+		ShadowFar /= ShadowFar.W;
+
 		Shadow = GetLightAmount(ShadowNear, ShadowFar);
 	}
     return float4(Shadow, Shadow, Shadow, 1.0f);
@@ -158,28 +159,21 @@ float4 Shadow( VSOUT IN ) : COLOR0 {
 
 float4 BlurPass(VSOUT IN, uniform float2 OffsetMask) : COLOR0
 {
-	float3 Color = 0.0f;
-	float w = 0.0f;
 	float depth = readDepth01(IN.UVCoord);
-	
-	if (depth == 0.0f || depth >= 0.9f) discard;
-    for (int i = 0; i < cKernelSize; i++) {
-		float2 uvOff = (BlurOffsets[i] * OffsetMask) * pow(abs(log(depth)), 4.0f) * 0.00125f;
-		Color += tex2D(TESR_RenderedBuffer, IN.UVCoord + uvOff).rgb * BlurWeights[i];
-		w += BlurWeights[i];
-    }
-	Color /= w;
-    return float4(Color, 1.0f);
+	if (depth == 0.0f || depth >= 0.9f) {
+		discard;
+	}
+
+    return float4(tex2D(TESR_RenderedBuffer, IN.UVCoord).rgb, 1.0f);
 }
 
 float4 CombineShadow( VSOUT IN ) : COLOR0 {
-
 	float3 color = tex2D(TESR_SourceBuffer, IN.UVCoord).rgb;
 	float Shadow = tex2D(TESR_RenderedBuffer, IN.UVCoord).r;
 	
 	color.rgb *= Shadow;
-    return float4(color, 1.0f);
-	
+
+    return float4(color, 1.0f);	
 }
 
 technique {
